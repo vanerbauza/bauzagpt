f8ef5666b0b60864f6634638d115e64745ee993a
ivan bauza <bauza723@gmail.com>
2025-10-14 16:32:04 -0600

    chore: mover archivos web a /docs

---
+++ docs/js/vite.config.js
@@ -0,0 +1,9 @@
+﻿import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/",                 // dominio propio ⇒ raíz
+  build: {
+    outDir: "docs",          // el build se publica en /docs
+    emptyOutDir: true
+  }
+});
